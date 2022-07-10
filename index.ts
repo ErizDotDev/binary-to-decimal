a8167e17f4be71d8ba278ced948c6721af3b9271
Denzel dela Cruz <denzeldevwork01@gmail.com>
2022-07-11 03:57:45 +0800

    add working unoptimized conversion function

--- index.ts
+++ index.ts
@@ -5,14 +5,61 @@ const main = (): void => {
     output: process.stdout,
   });
 
-  rl.question("Write anything: ", (anything: string) => {
-    console.log(`You've written ${anything}!`);
+  rl.question("Write an eight binary digit value: ", (binary: string) => {
+    const decimalResult = convertBinaryToDecimal(binary);
+    console.log(`Decimal value = ${decimalResult}!`);
     rl.close();
   });
 
   rl.on("close", () => {
     process.exit(0);
   });
+};
+
+const convertBinaryToDecimal = (binaryInput: string): number => {
+  const ones: number =
+    binaryInput.length >= 1
+      ? (binaryInput[binaryInput.length - 1] as unknown as number) * 1
+      : 0;
+  const twos: number =
+    binaryInput.length >= 2
+      ? (binaryInput[binaryInput.length - 2] as unknown as number) * 2
+      : 0;
+  const fours: number =
+    binaryInput.length >= 3
+      ? (binaryInput[binaryInput.length - 3] as unknown as number) * 4
+      : 0;
+  const eights: number =
+    binaryInput.length >= 4
+      ? (binaryInput[binaryInput.length - 4] as unknown as number) * 8
+      : 0;
+  const sixteens: number =
+    binaryInput.length >= 5
+      ? (binaryInput[binaryInput.length - 5] as unknown as number) * 16
+      : 0;
+  const thirtytwos: number =
+    binaryInput.length >= 6
+      ? (binaryInput[binaryInput.length - 6] as unknown as number) * 32
+      : 0;
+  const sixtyfours: number =
+    binaryInput.length >= 7
+      ? (binaryInput[binaryInput.length - 7] as unknown as number) * 64
+      : 0;
+  const oneTwentyEights: number =
+    binaryInput.length >= 8
+      ? (binaryInput[binaryInput.length - 78] as unknown as number) * 128
+      : 0;
+
+  return (
+    ones +
+    twos +
+    fours +
+    eights +
+    sixteens +
+    thirtytwos +
+    sixtyfours +
+    oneTwentyEights
+  );
 };
 
 main();
