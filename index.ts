9792df0f6e491f7ee4d0786fe495c097205b63ce
Denzel dela Cruz (CSV) <denzeleriz.delacruz@softvision.com>
2022-07-18 04:21:45 +0800

    optimize conversion method

--- index.ts
+++ index.ts
@@ -26,50 +26,18 @@ const main = (): void => {
   });
 };
 
-const convertBinaryToDecimal = (binaryInput: string): number => {
-  const ones: number =
-    binaryInput.length >= 1
-      ? (binaryInput[binaryInput.length - 1] as unknown as number) * 1
-      : 0;
-  const twos: number =
-    binaryInput.length >= 2
-      ? (binaryInput[binaryInput.length - 2] as unknown as number) * 2
-      : 0;
-  const fours: number =
-    binaryInput.length >= 3
-      ? (binaryInput[binaryInput.length - 3] as unknown as number) * 4
-      : 0;
-  const eights: number =
-    binaryInput.length >= 4
-      ? (binaryInput[binaryInput.length - 4] as unknown as number) * 8
-      : 0;
-  const sixteens: number =
-    binaryInput.length >= 5
-      ? (binaryInput[binaryInput.length - 5] as unknown as number) * 16
-      : 0;
-  const thirtyTwos: number =
-    binaryInput.length >= 6
-      ? (binaryInput[binaryInput.length - 6] as unknown as number) * 32
-      : 0;
-  const sixtyFours: number =
-    binaryInput.length >= 7
-      ? (binaryInput[binaryInput.length - 7] as unknown as number) * 64
-      : 0;
-  const oneTwentyEights: number =
-    binaryInput.length >= 8
-      ? (binaryInput[binaryInput.length - 8] as unknown as number) * 128
-      : 0;
+const convertBinaryToDecimal = (binaryInput: string, totalSum = 0): number => {
+  if (binaryInput.length === 0) {
+    return totalSum;
+  }
 
-  return (
-    ones +
-    twos +
-    fours +
-    eights +
-    sixteens +
-    thirtyTwos +
-    sixtyFours +
-    oneTwentyEights
-  );
+  const inputLength = binaryInput.length;
+  const multiplier = Math.pow(2, inputLength - 1);
+
+  totalSum += (binaryInput[0] as unknown as number) * multiplier;
+  binaryInput = binaryInput.substring(1);
+
+  return convertBinaryToDecimal(binaryInput, totalSum);
 };
 
 const cleanInput = (input: string): string => input.replace(" ", "");
